fc0aab80f924351ea8bf962d310df15e6ed8a0a8
Pratham1603 <prathamharer1603@gmail.com>
2026-06-11 16:20:05 +0530

    Requirements Added

---
+++ setup.py
@@ -0,0 +1,10 @@
+from setuptools import find_packages, setup
+
+setup(
+    name="sezzle-ai-customer-support-agent-chatbot",
+    version="0.1.0",
+    author="Pratham harer",
+    author_email="prathamharer1603@gmail.com",
+    packages=find_packages(),
+    install_requires=[]
+)
